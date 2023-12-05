eb32f5698870d97c4251d3cddad8ed372dae1e82
mirondorin <mirondorin99@gmail.com>
2023-12-05 10:06:46 +0200

    Added attribute icons

--- src/heroes/HeroesGrid.tsx
+++ src/heroes/HeroesGrid.tsx
@@ -1,5 +1,9 @@
 import heroes from '../assets/json/formatted_heroes.json';
 import './HeroesGrid.css';
+import strengthImage from '../assets/images/attributes/Strength-attribute.webp';
+import agilityImage from '../assets/images/attributes/Agility-attribute.webp';
+import intelligenceImage from '../assets/images/attributes/Intelligence-attribute.webp';
+import universalImage from '../assets/images/attributes/Universal-attribute.webp';
 
 function getHeroesByPrimaryAttribute(attribute: string) {
   return heroes
@@ -14,12 +18,28 @@ enum Attribute {
   all = 'UNIVERSAL',
 }
 
+function getAttributeImage(attribute: string) {
+  switch (attribute) {
+    case 'str':
+      return strengthImage;
+    case 'agi':
+      return agilityImage;
+    case 'int':
+      return intelligenceImage;
+    case 'all':
+      return universalImage;
+  }
+}
+
 export default function HeroesGrid({ attribute }: { attribute: string }) {
   const filteredHeroes = getHeroesByPrimaryAttribute(attribute);
 
   return (
     <div className="sorted-attribute-heroes">
-      <h1>{Attribute[attribute as keyof typeof Attribute]}</h1>
+      <div className='heroes-grid-header'>
+        <h1>{Attribute[attribute as keyof typeof Attribute]}</h1>
+        <img className='attribute-icon' src={getAttributeImage(attribute)}></img>
+      </div>
       <div className="heroes-grid-container">
         {filteredHeroes.map((hero) => (
           <img key={hero.id} className="hero-avatar" src={hero.image_url}></img>
